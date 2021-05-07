64d7593187bca2a90e670d43c186cd0a232c7de7
dean_lin <dean_lin@ubiqconn.com>
2021-05-07 16:21:25 +0800

    complete ch22

---
+++ ch22/tools/google_sheets/index.js
@@ -0,0 +1,89 @@
+require('dotenv').config(); //載入.env環境檔
+const fs = require('fs');
+const readline = require('readline');
+const { google } = require('googleapis');
+
+// If modifying these scopes, delete token.json.
+const SCOPES = ['https://www.googleapis.com/auth/spreadsheets.readonly'];
+// The file token.json stores the user's access and refresh tokens, and is
+// created automatically when the authorization flow completes for the first
+// time.
+// const TOKEN_PATH = 'token.json';
+const TOKEN_PATH = 'tools/google_sheets/token.json';
+// Load client secrets from a local file.
+//fs.readFile('credentials.json', (err, content) => {
+fs.readFile('tools/google_sheets/credentials.json', (err, content) => {
+  if (err) return console.log('Error loading client secret file:', err);
+  // Authorize a client with credentials, then call the Google Sheets API.
+  authorize(JSON.parse(content), listMySheet);
+});
+
+/**
+ * Create an OAuth2 client with the given credentials, and then execute the
+ * given callback function.
+ * @param {Object} credentials The authorization client credentials.
+ * @param {function} callback The callback to call with the authorized client.
+ */
+function authorize (credentials, callback) {
+  const { client_secret, client_id, redirect_uris } = credentials.installed;
+  const oAuth2Client = new google.auth.OAuth2(
+    client_id, client_secret, redirect_uris[0]);
+
+  // Check if we have previously stored a token.
+  fs.readFile(TOKEN_PATH, (err, token) => {
+    if (err) return getNewToken(oAuth2Client, callback);
+    oAuth2Client.setCredentials(JSON.parse(token));
+    callback(oAuth2Client);
+  });
+}
+
+/**
+ * Get and store new token after prompting for user authorization, and then
+ * execute the given callback with the authorized OAuth2 client.
+ * @param {google.auth.OAuth2} oAuth2Client The OAuth2 client to get token for.
+ * @param {getEventsCallback} callback The callback for the authorized client.
+ */
+function getNewToken (oAuth2Client, callback) {
+  const authUrl = oAuth2Client.generateAuthUrl({
+    access_type: 'offline',
+    scope: SCOPES,
+  });
+  console.log('Authorize this app by visiting this url:', authUrl);
+  const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout,
+  });
+  rl.question('Enter the code from that page here: ', (code) => {
+    rl.close();
+    oAuth2Client.getToken(code, (err, token) => {
+      if (err) return console.error('Error while trying to retrieve access token', err);
+      oAuth2Client.setCredentials(token);
+      // Store the token to disk for later program executions
+      fs.writeFile(TOKEN_PATH, JSON.stringify(token), (err) => {
+        if (err) return console.error(err);
+        console.log('Token stored to', TOKEN_PATH);
+      });
+      callback(oAuth2Client);
+    });
+  });
+}
+
+async function listMySheet (auth) {
+  const sheets = google.sheets({ version: 'v4', auth });
+  const title = '工作表1'//請你更改成自己設定的sheet(工作表)名稱
+  const request = {
+      spreadsheetId: process.env.SPREADSHEET_ID,
+      range: [
+          //這是指抓取的範圍，你也可以改寫成A1:A300(抓第1欄的第1列到第300列)
+          `'${title}'!A:ZZ`
+      ],
+      valueRenderOption: "FORMULA"//FORMATTED_VALUE|UNFORMATTED_VALUE|FORMULA
+  }
+  try {
+      //這裡改寫為await，之後會有順序執行的需求
+      let values = (await sheets.spreadsheets.values.get(request)).data.values;
+      console.log(values)
+  } catch (err) {
+      console.error(err);
+  }
+}
--- ch22/tools/initDrive.js
+++ ch22/tools/initDrive.js
@@ -1,9 +1,22 @@
+exports.initDrive = initDrive;//讓其他程式在引入時可以使用這個函式
+
 const webdriver = require('selenium-webdriver') // 加入虛擬網頁套件
 const chrome = require('selenium-webdriver/chrome');
 const options = new chrome.Options();
 options.addArguments('--log-level=3')//這個option可以讓你跟網頁端的console.log說掰掰
 options.setUserPreferences({ 'profile.default_content_setting_values.notifications': 1 });//因為FB會有notifications干擾到爬蟲，所以要先把它關閉
-options.addArguments("user-agent='Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/74.0.3729.169 Safari/537.36'")
+if (process.env.HIDEIMAGE === 'true') {
+  //不載入圖片
+  options.addArguments('blink-settings=imagesEnabled=false')
+}
+if (process.env.HEADLESS === 'true') {
+  options.addArguments('--headless')//不開啟瀏覽器
+  //下面參數能提升爬蟲穩定性    
+  options.addArguments('--disable-dev-shm-usage')//使用共享內存RAM
+  options.addArguments('--disable-gpu')//規避部分chrome gpu bug
+  //偽裝瀏覽器身份
+  options.addArguments("user-agent='Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/74.0.3729.169 Safari/537.36'")
+}
 const path = require('path');//用於處理文件路徑的小工具
 const fs = require("fs");//讀取檔案用
 
@@ -18,8 +31,9 @@ async function initDrive () {
     ).build();
 
   //考慮到ig在不同螢幕寬度時的Xpath不一樣，所以我們要在這裡設定統一的視窗大小
-  await driver.manage().window().setRect({ width: 800, height: 720, x: 0, y: 0 });
-  driver.get("https://gs.statcounter.com/detect")
+  await driver.manage().window().setRect({ width: 1280, height: 800, x: 0, y: 0 });
+
+  return driver
 }
 
 function checkDriver () {
@@ -40,4 +54,3 @@ function checkDriver () {
   }
   return true
 }
-initDrive()
