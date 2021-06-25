93d9a3b54b1f189c15fc5fa05c7041b44253d6ce
dean_lin <dean_lin@ubiqconn.com>
2021-06-25 20:32:35 +0800

    修改備註

--- ch7/index.js
+++ ch7/index.js
@@ -1,6 +1,6 @@
 require('dotenv').config(); //載入.env環境檔
 function getEnvVariable () {
   const env_variable = process.env.YOUR_VARIABLE; // 取出環境變數
-  console.log(env_variable);
+  console.log(env_variable); // 輸出環境變數
 }
 getEnvVariable();
